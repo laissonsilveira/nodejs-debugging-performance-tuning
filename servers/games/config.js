a1f4b582bd9ae672e8913dbbc2396629ea8138ab
Laisson R. Silveira <laisson.r.silveira@gmail.com>
2021-12-01 02:50:43 -0300

    Change config db

--- servers/games/config.js
+++ servers/games/config.js
@@ -1,21 +1,12 @@
-const connection = process.env.NODE_ENV === 'production'
-  ? {
-    host: 'us-cdbr-east-04.cleardb.com?reconnect=true',
-    port: '3306',
-    user: 'bc0ee671a70f6c',
-    password: 'fedc4a4b',
-    database: 'heroku_7ebab73c6c36ee2',
-  }
-  : {
-    host: '127.0.0.1',
-    port: '33306',
-    user: 'root',
-    database: 'rps',
-  };
 module.exports = {
   database: {
     client: 'mysql',
-    connection,
+    connection: process.env.CLEARDB_DATABASE_URL || {
+      host: '127.0.0.1',
+      port: '33306',
+      user: 'root',
+      database: 'rps',
+    },
     migrations: {
       tableName: '_knex_migrations',
     },
