b6ce4d383d511a51ec9424c1d988a056c5b900f9
Laisson R. Silveira <laisson.r.silveira@gmail.com>
2021-12-01 02:43:33 -0300

    update

--- servers/players/config.js
+++ servers/players/config.js
@@ -1,12 +1,21 @@
+const connection = process.env.NODE_ENV === 'production'
+  ? {
+    host: 'us-cdbr-east-04.cleardb.com?reconnect=true',
+    port: '3306',
+    user: 'bc0ee671a70f6c',
+    password: 'fedc4a4b',
+    database: 'heroku_7ebab73c6c36ee2',
+  }
+  : {
+    host: '127.0.0.1',
+    port: '33306',
+    user: 'root',
+    database: 'rps',
+  };
 module.exports = {
   database: {
     client: 'mysql',
-    connection: {
-      host: '127.0.0.1',
-      port: '33306',
-      user: 'root',
-      database: 'rps',
-    },
+    connection,
     migrations: {
       tableName: '_knex_migrations',
     },
